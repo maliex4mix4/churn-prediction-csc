aa529ffb99280d4081e9abb19377187dc682d102
Malik Diyaolu <49034182+maliex4mix4@users.noreply.github.com>
2023-06-15 21:20:32 +0100

    Update __init__.py

--- ms/__init__.py
+++ ms/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import pickle
+from flask_cors import CORS
 
 
 # Initialize App
 app = Flask(__name__)
+CORS(app)
 
 # Load models
 model = pickle.load(open('model/model.pkl', 'rb'))
